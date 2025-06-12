f56c5d486eeaf4c4ee0068dd201a508517f4095f
ardasaritas <143201588+ardasaritas@users.noreply.github.com>
2025-06-13 00:50:27 +0300

    updated release version

--- claw.rb
+++ claw.rb
@@ -1,8 +1,8 @@
 class Claw < Formula
   desc "VS Code-based C/C++ development launcher with debugging, tasks, and automation"
   homepage "https://github.com/ardasaritas/claw"
-  url "https://github.com/ardasaritas/claw/releases/download/v1.0.1/claw-1.0.1.tar.gz"
-  sha256 "232c878fa5447592f925c90996d9067622f3a1e997e21a57f7154ff3503f7efa"
+  url "https://github.com/ardasaritas/claw/releases/download/v1.0.2/claw-1.0.2.tar.gz"
+  sha256 "37a9d3ae79a88812cd6d38df1ff57a367c37c44aac1c665be5ab83135121cf1c"
   license "MIT"
 
   def install
